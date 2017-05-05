344ab753d8294d562a9d2a656e3eee2183e95a16
Nick Sayer <nsayer@kfu.com>
2017-05-04 20:46:38 -0700

    Reformat the PRR stuff to make it just a little more obvious what’s different with the #ifdefs.

--- Orthrus.c
+++ Orthrus.c
@@ -385,20 +385,26 @@ void __ATTR_NORETURN__ main(void) {
 	CLK.USBCTRL = CLK_USBSRC_PLL_gc | CLK_USBSEN_bm; // USB is clocked from the PLL.
 
 	// turn off the bits of the chip we don't need.
-	PR.PRGEN = PR_RTC_bm | PR_EBI_bm | PR_EVSYS_bm; // EBI is probably moot.
+	PR.PRGEN = PR_RTC_bm | PR_EBI_bm | PR_EVSYS_bm; // EBI is probably moot for this chip variant.
 	PR.PRPA = PR_DAC_bm | PR_ADC_bm | PR_AC_bm; // all analog stuff off.
 	PR.PRPB = PR_DAC_bm | PR_ADC_bm | PR_AC_bm; // all analog stuff off.
+
+	PR.PRPC = PR_TWI_bm | PR_USART1_bm | PR_SPI_bm | PR_HIRES_bm | PR_TC1_bm
 #ifdef USART_SPI
-	PR.PRPC = PR_TWI_bm | PR_USART1_bm | PR_SPI_bm | PR_HIRES_bm | PR_TC1_bm;
+										 | PR_SPI_bm
 #else
-	PR.PRPC = PR_TWI_bm | PR_USART0_bm | PR_USART1_bm | PR_HIRES_bm | PR_TC1_bm;
+										 | PR_USART0_bm
 #endif
+		;
+
+
+	PR.PRPE = PR_TWI_bm | PR_USART1_bm | PR_SPI_bm | PR_HIRES_bm | PR_TC1_bm | PR_TC0_bm
+#ifndef DEBUG
+											     | PR_USART0_bm
+#endif
+		;
+
 	PR.PRPD = PR_TWI_bm | PR_USART0_bm | PR_USART1_bm | PR_SPI_bm | PR_HIRES_bm | PR_TC1_bm | PR_TC0_bm;
-#ifdef DEBUG
-	PR.PRPE = PR_TWI_bm | PR_USART1_bm | PR_SPI_bm | PR_HIRES_bm | PR_TC1_bm | PR_TC0_bm;
-#else
-	PR.PRPE = PR_TWI_bm | PR_USART0_bm | PR_USART1_bm | PR_SPI_bm | PR_HIRES_bm | PR_TC1_bm | PR_TC0_bm;
-#endif
 	PR.PRPF = PR_TWI_bm | PR_USART0_bm | PR_USART1_bm | PR_SPI_bm | PR_HIRES_bm | PR_TC1_bm | PR_TC0_bm;
 
 	init_ports();
