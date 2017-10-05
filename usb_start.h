90ef6d537b311fb7e35e8f99b1ef5d48c858e1e3
Nick Sayer <nsayer@users.noreply.github.com>
2017-10-05 08:25:33 -0700

    Update usb_start.h
    This isn't really a "Start" generated file anymore.

--- usb_start.h
+++ usb_start.h
@@ -1,10 +1,22 @@
 /*
- * Code generated from Atmel Start.
- *
- * This file will be overwritten when reconfiguring your Atmel Start project.
- * Please copy examples or other code you want to keep to a separate file or main.c
- * to avoid loosing it when reconfiguring.
+
+ Copyright 2017 Nicholas W. Sayer
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+
 #ifndef USB_DEVICE_MAIN_H
 #define USB_DEVICE_MAIN_H
 
